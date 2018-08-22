d977d287c93e43609f6560fac20c7bd5ccd4063d
David McGrann <dpmcgrann@gmail.com>
2018-08-22 17:16:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id
+  
     if @user == nil 
       redirect 'sessions/login'
     else
